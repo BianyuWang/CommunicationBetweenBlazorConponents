3be877ff413b7096b5d42d0db995724c601c3a85
Bian Yu Wang <bianyu.wang@gmail.com>
2022-06-16 14:36:22 -0400

    Add project files.

---
+++ CommunicationBetweenConponents/Data/NotifierService.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CommunicationBetweenConponents.Data
+{
+    public class NotifierService
+    {
+        public int PinkScale { get; private set; }
+        public event Action OnScaleChange;
+
+        public void SetPattern(int newPinkScale)
+        {
+            PinkScale = newPinkScale;
+            NotifyStateChanged();
+        }
+
+        private void NotifyStateChanged() => OnScaleChange?.Invoke();
+    }
+}
